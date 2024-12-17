43c2423d826389ac4e7d50e41c49deedc68b9116
Ahmedalihh <ahmedalibbb227@gmail.com>
2024-12-17 23:12:11 +0500

    learning js

---
+++ app.js
@@ -0,0 +1,100 @@
+// var username = "ahmed ali"; string
+// console.log(typeof username);
+
+// var num1 = 500; //number data type
+// console.log(typeof (num1))
+
+// var useremail = null;
+
+// var result = undefined;
+
+// Boolean = true, false
+
+// var isactive = true;
+// boolean;
+// console.log(typeof isactive)
+
+// +, -, * , / arithmatic opreators
+// var num1 = 31;
+// var num2 = 3;
+// var add = num1 + num2;
+// console.log("add", add);
+// var num1 = 100;
+// var num2 = 200;
+// var sub = num1 - num2;
+// console.log("sub", sub);
+
+// var num1 = 100;
+// var num2 = 200;
+// var mul = num1 * num2;
+// console.log("mul", mul);
+
+// var num1 = 100;
+// var num2 = 200;
+// var div = num1 / num2;
+// console.log("div", div);
+
+// % => modulous
+
+// var per = num1 % num2;
+// console.log("per", per);
+
+// rule BODMAS
+// var num1 = 5;
+
+// var num2 = 10;
+
+// var result = num1 + num2 * num2;
+// console.log("result", result);
+
+// + => concatination
+
+// var num1 = "20"; //string
+// var num2 = "80"; //string
+// var num2 = 80;
+// var num1 = 20;
+// console.log(num1 + num2);
+
+// var num2 = "80";
+// var num1 = 20;
+// console.log(num1 - num2);
+
+// var num2 = "80a";
+// var num1 = 20;
+// console.log(num1 - num2);
+
+// var firstname = "ahmed";
+// var lastname = "ali";
+// var fullname = firstname + lastname;
+// var fullname = firstname + " " + lastname;
+// console.log(fullname);
+
+// var num1 = 10;
+// var num2 = 20;
+
+// var num3 = 30;
+// console.log(num1 + num2 + num3);
+
+// var num1 = 10;
+// var num2 = 20;
+
+// var num3 = "30";
+// console.log(num1 + num2 + num3);
+
+// var num1 = 10;
+// var num2 = "20";
+
+// var num3 = 30;
+// console.log(num1 + num2 + num3);
+
+// var num1 = "10";
+// var num2 = 20;
+
+// var num3 = 30;
+// console.log(num1 + num2 + num3);
+
+// var num1 = "10";
+// var num2 = 20;
+
+// var num3 = 30;
+// console.log(num1 + (num2 + num3));
